393f264284667e4d860bdea6835685539fd98691
amusing-transpiler-2573 <anil_grg@hotmail.com>
2019-12-30 05:47:33 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -10,11 +10,13 @@ return result
 end
 
 def division(num1, num2)
+  result= (num1/num2)
+  return result
 
 end
 
 def multiplication(num1, num2)
-
+result = (num1*num2)
 end
 
 def modulo(num1, num2)
